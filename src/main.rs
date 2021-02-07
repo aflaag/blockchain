b0f7e1a018dd8ce63f72dd715a6282e6106cde0c
ph04 <47356461+ph04@users.noreply.github.com>
2021-02-07 17:52:45 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
-mod block;
+mod account;
+mod positive_f64;
 mod transaction;
+mod block;
 mod blockchain;
 
 use crate::{
 	transaction::Transaction,
-	block::Block,
 	blockchain::BlockChain,
+	account::Account,
 };
 
+// TODO:
+// capire come fare i test negli esempi
+// far funzionare i test
+// aggiungere il tempo di creazione di tutte le cose che lo richiedono
+//     modificare di conseguenza i vari hash
+// effettua transazione
+// RISOLVI PROBLEMA &mut Account
+// impl sub pf64
+
 fn main() {
-	let t0 = Transaction::new("a", "b", 0);
-	let t1 = Transaction::new("c", "d", 1);
-	let t2 = Transaction::new("e", "f", 2);
-	let t3 = Transaction::new("g", "h", 3);
-	let t4 = Transaction::new("i", "j", 4);
-	let t5 = Transaction::new("k", "l", 5);
-	let t6 = Transaction::new("m", "n", 6);
-	let t7 = Transaction::new("o", "p", 7);
-	let t8 = Transaction::new("q", "r", 8);
-	
+	let a0 = Account::new("a", "a", "a");
+	let a1 = Account::new("b", "b", "b");
+	let a2 = Account::new("c", "c", "c");
+	let a3 = Account::new("d", "d", "d");
+	let a4 = Account::new("e", "e", "e");
+	let a5 = Account::new("f", "f", "f");
+
+	let t0 = Transaction::new(a0, a1, 5.1, "a"); // THEY ARE NOT UPDATED
+	let t1 = Transaction::new(a2, a3, 1.0, "c");
+	let t2 = Transaction::new(a4, a5, -2.0, "e");
+
 	let mut blockchain = BlockChain::new(2);
 	blockchain.push_transaction(t0);
 	blockchain.push_transaction(t1);
 	blockchain.push_transaction(t2);
-	blockchain.push_transaction(t3);
-	blockchain.push_transaction(t4);
-	blockchain.push_transaction(t5);
-	blockchain.push_transaction(t6);
-	blockchain.push_transaction(t7);
-	blockchain.push_transaction(t8);
 
 	println!("{:#?}", blockchain);
+	//println!("{}, {}", a0, a1)
 }
